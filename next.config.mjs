1159f60f6c997020900b38afa2010c84c26556df
Ahmed Nour <darklevel152@gmail.com>
2024-10-30 10:32:28 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "",
-  reactStrictMode: true,
-  // trailingSlash: true,
+ 
   images: {
     loader: "custom",
     loaderFile: "./ImageLoader.js",
