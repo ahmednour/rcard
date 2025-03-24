f3261bf0f0bee5b49d94899ec8ab336b63b73ccf
Ahmed Nour <darklevel152@gmail.com>
2025-03-25 00:10:06 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,12 +3,15 @@ import "./globals.css";
 export const metadata = {
   title: "كارت معايدة",
   description: "كارت معايدة أمانه منطقة نجران",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="dark:bg-[#dbdfe0]">{children}</body>
+    <html lang="ar" dir="rtl">
+      <body className="dark:bg-[#dbdfe0] min-h-screen w-full overflow-x-hidden">
+        {children}
+      </body>
     </html>
   );
 }
